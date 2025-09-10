0903f78663ed487dea1f33737b58f50b3b88946d
Viscous106 <virulkaryashed@gmail.com>
2025-09-10 11:25:32 +0530

    starting javascript

--- Web-dev-notes/Script.js
+++ Web-dev-notes/Script.js
@@ -1,1 +1,22 @@
-console.log('Backend works here')
+/*
+console.log('Backend works here')// This is a print statement that prints on the console.
+alert('Welcome to the site!!') // This is an alert or basically this popup that appears on screen.
+
+
+//Variables in JS:
+var name = 'Viscous'; // var is a variable that can be changed later in the code.
+let age = 19; // let is also a variable that can be changed later in the code.
+const pi = 3.14; // const is a variable that cannot be changed later in the code.
+
+console.log(name);
+console.log(age);
+console.log(pi);
+*/
+
+var name = 'Viscous'; // var is a variable that can be changed later in the code.
+let age = 19; // let is also a variable that can be changed later in the code.
+const pi = 3.14; // const is a variable that cannot be changed later in the code.
+
+console.log(name);
+console.log(age);
+console.log(pi);
