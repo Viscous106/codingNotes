f19504671170121cada9e4cca9a4aa588beb735d
Viscous106 <virulkaryashed@gmail.com>
2025-09-12 23:35:15 +0530

    Bash started

--- Bash/Notes.sh
+++ Bash/Notes.sh
@@ -3,7 +3,20 @@
 # ./filePath  use this to run the file 
 
 # Single line comment
+: '
+Multi line comment
+'
 
-
+: '
 echo "hello world!!" ##Print Statement
+'
 
+###Conditionals:
+a=10
+if [ $a -eq 10 ]; then # Check if a is equal to 10
+    echo "a is 10"
+elif [ $a -ne 10 ]; then # Check if a is not equal to 10
+    echo "a is not 10"
+else
+    echo "maths isnt mathing anymore"
+fi
