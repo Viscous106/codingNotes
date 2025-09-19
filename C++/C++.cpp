31fc03adb3e87ff1cd342a29d0374bd0ea573748
Viscous106 <Virulkaryashed@gmail.com>
2025-09-19 12:58:03 +0530

    Untill IF ELSE updated

--- C++/C++.cpp
+++ C++/C++.cpp
@@ -93,9 +93,53 @@ int main() {
         cout << sqrt(64);
         cout << round(2.6);
         cout << log(2);
+    //If Else Else if:
+        int x = 20;
+        int y = 18;
+        if (x > y) {
+            cout << "x is greater than y";
+        }
+        else if (x == y) {
+            cout << "x and y are equal";
+        }
+        else {
+            cout << "x is less than y";
+        }
+    //Shortcut if else:
+        int time = 20;
+        string result = (time < 18) ? "Good day." : "Good evening.";
+        cout << result;
+    //Switch:
+        int day = 4;
+        switch (day) {
+            case 1:
+                cout << "Monday";
+                break;
+            case 2:
+                cout << "Tuesday";
+                break;
+            case 3:
+                cout << "Wednesday";
+                break;
+            case 4:
+                cout << "Thursday";
+                break;
+            case 5:
+                cout << "Friday";
+                break;
+            case 6:
+                cout << "Saturday";
+                break;
+            case 7:
+                cout << "Sunday";
+                break;}
+            default:
+                cout << "Lol!!";
+    //While Loop:
+        
 
 }
 */
 int main() {
-    
+
 }
