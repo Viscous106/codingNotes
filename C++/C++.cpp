a4e5b320b1ecb4ce9915f6d3e4ff6a34993df194
Viscous106 <Virulkaryashed@gmail.com>
2025-09-19 12:02:26 +0530

    minor updates

--- C++/C++.cpp
+++ C++/C++.cpp
@@ -1,8 +1,75 @@
 #include <iostream>//Header file
 using namespace std;
 
+/*
 int main() {
-    //Print Statement
-    cout << "Hello Arch!";
-    return 0;
+    //Comments:
+        //Single line comment
+        Multi line comment
+
+    //Print Statement:
+        cout << "Hello Arch!"<<"\n";//Str //\n used to make new line.
+        cout << 45 <<"\n";//num
+
+    //Variables:
+        int a = 5;//int - stores integers (whole numbers), without decimals, such as 123 or -123
+        float myFloatNum = 5.99;     // Floating point number
+        double b = 57.8;//double - stores floating point numbers, with decimals, such as 19.99 or -19.99
+        char c = 'g';//char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+        string d = "Arch";//string - stores text, such as "Hello World". String values are surrounded by double quotes
+        bool e=true;//bool - stores values with two states: true or false
+        const f=8//const - can't change value
+        cout << "I am using " << d<<"\n";
+        auto x = "Something" //Auto detects the data type 
+
+    //Operations:
+        //addn:
+        int sum = a+b;
+        cout << sum <<"\n";
+        //Subt:
+        int sub = a-b;
+        cout << sub <<"\n";
+        //Mult:
+        int mult = a*b;
+        cout << mult <<"\n";
+        //Div:
+        int div = a/b;
+        cout << div <<"\n";
+        //Mod:
+        int mod = a%2;
+        cout << mod <<"\n";
+
+    //Declaring multiple variables:
+        int a=2,b=4,c=3;
+        cout<<"a="<<a<<"\n"<<"b="<<b<<"\n"<<"c="<<c<<"\n";
+    
+    //Taking Input:
+        cout<<"Type an int input:"<<"\n";
+        int a;
+        cin>>a;
+        cout<<"Type a double input:"<<"\n";
+        double b;
+        cin>>b;
+        cout<<"Type a char input:"<<"\n";
+        char c;
+        cin>>c;
+        cout<<"Type a string input:"<<"\n";
+        string d;
+        cin>>d;
+        cout<<"Type a bool input:"<<"\n";
+        bool e;
+        cin>>e;
+        cout<<"a="<<a<<"\n"<<"b="<<b<<"\n"<<"c="<<c<<"\n"<<"d="<<d<<"\n"<<"e="<<e<<"\n";
+
+    
+}
+*/
+int main() {
+    //Concatination:
+        string a="Arch";
+        string b="Linux";
+        string c=a+" "+b;
+        cout<<c<<"\n";
+
+
 }
