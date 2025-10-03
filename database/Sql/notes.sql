17aa6ecb9a291fe003fb5b3737208cff3effb1b6
Viscous <virulkaryashed@gmail.com>
2025-10-03 20:49:56 +0530

    push

--- database/Sql/notes.sql
+++ database/Sql/notes.sql
@@ -1,13 +1,16 @@
-CREATE TABLE users(
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    username TEXT NOT NULL UNIQUE,
-    email TEXT,
-    age INTEGER,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
+-- CREATE TABLE users(
+--     id INTEGER PRIMARY KEY,
+--     name TEXT NOT NULL,
+--     username TEXT NOT NULL UNIQUE,
+--     email TEXT,
+--     age INTEGER,
+--     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+-- );
+
+--INSERT INTO users (name, username)
+--VALUES ('Rajnish', 'bkl_rajnish');      
+
+--SELECT * FROM users;
 
 INSERT INTO users (name, username)
-VALUES ('Rajnish', 'bkl_rajnish');      
-
-SELECT * FROM users;
+VALUES ('Rajnish', 'bkl_rajnish'),('John Doe', 'jdoe');
