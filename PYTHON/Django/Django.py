027ea96dcf43e7f743df20d50c318b2dbd857f6f
Viscous <virulkaryashed@gmail.com>
2025-11-13 11:39:56 +0530

    Started with django

---
+++ PYTHON/Django/Django.py
@@ -0,0 +1,9 @@
+
+###Intro to django:
+'''
+make a django project and app
+1. pip install django
+2. django-admin startproject projectname
+3. cd projectname
+4. python manage.py startapp appname
+'''
