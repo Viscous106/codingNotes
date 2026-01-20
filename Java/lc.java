a886936a110a0f356634d4e35d2f8b8f54c4c95f
Viscous106 <virulkaryashed@gmail.com>
2026-01-20 14:17:18 +0530

    udt:new notes

--- Java/lc.java
+++ Java/lc.java
@@ -1,12 +1,5 @@
 public class lc {
-	static void P(int n){
-		System.out.print(n);
-		if (n==1)
-			return;	
-		P(n-1);
-	}
-	public static void main(String[] args) {
-		P(3);
-	}
+    public static void main(String[] args) {
+		
+    }
 }
-
