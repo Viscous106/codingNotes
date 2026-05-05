83e39aa6ee756df8994f39b3ccbef5ce585699df
Yash Nitin Virulkar <virulkaryashed@gmail.com>
2026-05-05 09:54:12 +0530

    rm bootdev practise area

--- database/Sql/bootdev/learn-sql/001_up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
-  name TEXT NOT NULL,
-  age INTEGER NOT NULL,
-  country_code TEXT NOT NULL,
-  username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL,
-  is_admin BOOLEAN
-);
--- database/Sql/bootdev/learn-sql/002_main.sql
+++
@@ -1,1 +0,0 @@
--- Write your INSERT statements here
