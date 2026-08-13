f3ec7e04fa4c1db13012812fa95fb1f0a6dc6cae
Yash Nitin Virulkar <virulkaryashed@gmail.com>
2026-08-13 13:09:56 +0530

    notes(mern-stack): how to get an status code for debugging
    Signed-off-by: Yash Nitin Virulkar <virulkaryashed@gmail.com>

--- mern/backend/server.js
+++ mern/backend/server.js
@@ -1,7 +1,19 @@
 import express from "express"
-//const express = require("express")
+// const express = require("express")
 
 const app = express() // creating a basic app 
+
+// first get request:
+// 200 returns if it resolves safely.
+app.get("/api/notes", (req, res) => {
+	res.status(200).send("you got your first response");
+})
+
+// first post request:
+// 201 if creation was done success.
+app.post("/api/notes", (req, res) => {
+	res.status(201).send("your first request is changed");
+})
 
 app.listen(5006, () => {
 	console.log("Server started on PORT: 5006");
