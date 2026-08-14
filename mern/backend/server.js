b273dcf4f78e0cbd6a70b5d497cc0ad12797ee79
Yash Nitin Virulkar <virulkaryashed@gmail.com>
2026-08-14 10:49:22 +0530

    notes(mern-stack): how to use all kind of api calls
    Signed-off-by: Yash Nitin Virulkar <virulkaryashed@gmail.com>

--- mern/backend/server.js
+++ mern/backend/server.js
@@ -12,7 +12,15 @@ app.get("/api/notes", (req, res) => {
 // first post request:
 // 201 if creation was done success.
 app.post("/api/notes", (req, res) => {
-	res.status(201).send("your first request is changed");
+	res.status(201).json({ message: "notes created succesfully" });
+})
+
+app.put("/api/notes/:id", (req, res) => {
+	res.status(200).json({ message: "notes updated successfully" });
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+	res.status(200).json({ message: "notes deleted succefully" });
 })
 
 app.listen(5006, () => {
