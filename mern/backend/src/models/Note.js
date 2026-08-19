18bae694052517825842009acaea1df05336e902
Yash Nitin Virulkar <virulkaryashed@gmail.com>
2026-08-19 10:49:19 +0530

    notes(mern): how to make a db schema
    Signed-off-by: Yash Nitin Virulkar <virulkaryashed@gmail.com>

---
+++ mern/backend/src/models/Note.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+
+// Schema:
+const noteSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true
+		},
+		content: {
+			type: String,
+			reqired: true
+		},
+	},
+	{ timeStamps: true }// createdAt ,updatedAt
+);
+
+// Model:
+const Note = mongoose.model("Notes", noteSchema);
+export default Note
