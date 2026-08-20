b97eacf6d0fd9354e20d35534af8e34ce650a295
Yash Nitin Virulkar <virulkaryashed@gmail.com>
2026-08-20 17:03:48 +0530

    notes(mern): Cleaning
    Signed-off-by: Yash Nitin Virulkar <virulkaryashed@gmail.com>

--- mern/frontend/src/App.jsx
+++ mern/frontend/src/App.jsx
@@ -8,7 +8,6 @@ import { useToasterStore } from "react-hot-toast/headless"
 const App = () => {
 	return (
 		<div>
-			<button onClick={() => toast.success("shabbash")} className="text-red-500 p-4 bg-pink-300"> button daba mc </button>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/create" element={<CreatePage />} />
