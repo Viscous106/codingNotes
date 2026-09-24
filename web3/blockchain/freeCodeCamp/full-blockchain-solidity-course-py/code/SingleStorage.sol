6fe7ec9e77829fe64c2d519dffaa8235bd08ea94
Yash Nitin Virulkar <virulkaryashed@gmail.com>
2026-09-24 17:24:09 +0530

    notes: Starting with solidity
    Signed-off-by: Yash Nitin Virulkar <virulkaryashed@gmail.com>

---
+++ web3/blockchain/freeCodeCamp/full-blockchain-solidity-course-py/code/SingleStorage.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.6.0 <0.9.0;
+
+contract SimpleStorage {
+    uint256 abhinavsheight = 5;
+    bool isSohamGay = true;
+    string message = "maybe this is too stupid";
+    int256 negative = -1; //can be both positive and negative
+    address ethereumAddAcc2 = 0x1725a458a3Fc8BB3d7460d262c7F94766FEF585D;
+    bytes32 favouriteBytes = "cat"; // 0x6361740000000000000000000000000000000000000000000000000000000000
+}
+
